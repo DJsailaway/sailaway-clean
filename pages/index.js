b7c6b3f20b39959783548aa1ea3deccf08796b1d
DJsailaway <david@stanthony.co.uk>
2026-04-05 10:30:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -82,15 +82,17 @@ return (
     </div>
 
     {/* Links */}
-    <div style={{ display: "flex", gap: "20px", fontSize: "0.95rem" }}>
-      <a href="/" style={{ textDecoration: "none", color: "#333" }}>Home</a>
-      <a href="/motor-boat-hire-helford" style={{ textDecoration: "none", color: "#333" }}>Motor Boats</a>
-      <a href="/sailing-boat-hire-helford">Sailing</a>
-      <a href="/kayak-hire-helford">Kayaks</a>
-      <a href="/st-anthony-helford-river">Location</a>
-      <a href="/boatyard-services" style={{ textDecoration: "none", color: "#333" }}>Boatyard</a>
-      <a href="/#booking" style={{ textDecoration: "none", color: "#1e3a5f", fontWeight: "bold" }}>Book</a>
-    </div>
+<div style={{ display: "flex", gap: "20px", fontSize: "0.95rem" }}>
+  <a href="/" style={{ textDecoration: "none", color: "#333" }}>Home</a>
+  <a href="/motor-boat-hire-helford" style={{ textDecoration: "none", color: "#333" }}>Motor Boats</a>
+  <a href="/sailing-boat-hire-helford">Sailing</a>
+  <a href="/kayak-hire-helford">Kayaks</a>
+  <a href="/st-anthony-helford-river">Location</a>
+  <a href="/boat-launching-helford" style={{ textDecoration: "none", color: "#333" }}>Boat Launching</a>
+  <a href="/boat-storage-helford" style={{ textDecoration: "none", color: "#333" }}>Boat Storage</a>
+  <a href="/gillan-creek-moorings" style={{ textDecoration: "none", color: "#333" }}>Moorings</a>
+  <a href="/#booking" style={{ textDecoration: "none", color: "#1e3a5f", fontWeight: "bold" }}>Book</a>
+</div>
 
   </div>
 
