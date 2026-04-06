b9f9ce13173bcccb75b761fa116b8a1856394460
DJsailaway <david@stanthony.co.uk>
2026-04-06 17:42:54 +0100

    Update gillan-creek-moorings.js

--- pages/gillan-creek-moorings.js
+++ pages/gillan-creek-moorings.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navbar from "../components/navbar";
 
 export default function Moorings() {
   return (
@@ -12,37 +13,7 @@ export default function Moorings() {
       </Head>
 
   {/* NAVBAR */}
-  <div style={{
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "20px",
-    borderBottom: "1px solid #eee",
-    backgroundColor: "white",
-    position: "sticky",
-    top: 0,
-    zIndex: 1000
-  }}>
-    
-    {/* Logo / Brand */}
-    <div style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
-      Sailaway
-    </div>
-
-    {/* Links */}
-<div style={{ display: "flex", gap: "20px", fontSize: "0.95rem" }}>
-  <a href="/" style={{ textDecoration: "none", color: "#333" }}>Home</a>
-  <a href="/motor-boat-hire-helford" style={{ textDecoration: "none", color: "#333" }}>Motor Boats</a>
-  <a href="/sailing-boat-hire-helford">Sailing</a>
-  <a href="/kayak-hire-helford">Kayaks</a>
-  <a href="/st-anthony-helford-river">Location</a>
-  <a href="/boat-launching-helford" style={{ textDecoration: "none", color: "#333" }}>Boat Launching</a>
-  <a href="/boat-storage-helford" style={{ textDecoration: "none", color: "#333" }}>Boat Storage</a>
-  <a href="/gillan-creek-moorings" style={{ textDecoration: "none", color: "#333" }}>Moorings</a>
-  <a href="/#booking" style={{ textDecoration: "none", color: "#1e3a5f", fontWeight: "bold" }}>Book</a>
-</div>
-
-  </div>
+      <Navbar />
 
 
       <div style={{ fontFamily: "sans-serif", padding: "40px 20px", maxWidth: "900px", margin: "0 auto" }}>
