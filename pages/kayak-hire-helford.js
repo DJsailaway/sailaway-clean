ead873bdab670716f50a1bc1601b3c806d1067aa
DJsailaway <david@stanthony.co.uk>
2026-04-11 11:25:13 +0100

    Update kayak-hire-helford.js

--- pages/kayak-hire-helford.js
+++ pages/kayak-hire-helford.js
@@ -38,7 +38,7 @@ export default function KayakHire() {
           <div style={{
             position: "absolute",
             inset: 0,
-            background: "linear-gradient(to bottom, rgba(0,0,0,0.2), rgba(0,0,0,0.7))"
+            background: "linear-gradient(to bottom, rgba(0,0,0,0.0), rgba(0,0,0,0.2))"
           }} />
 
           {/* Text */}
