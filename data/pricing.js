0c5a300d4ffbab4588a6092e0e64f71c1fdc82cb
DJsailaway <david@stanthony.co.uk>
2026-04-19 19:43:29 +0100

    Create pricing.js

---
+++ data/pricing.js
@@ -0,0 +1,61 @@
+export const PRICING = {
+  "Plymouth Pilot (8 people)": {
+    hourly: {
+      1: 120,
+      2: 200,
+      3: 270,
+      4: 320,
+    },
+    daily: 600,
+  },
+
+  "Bass Boat (5 people)": {
+    hourly: {
+      1: 100,
+      2: 180,
+      3: 240,
+      4: 300,
+    },
+    daily: 500,
+  },
+
+  "Drascombe Longboat (6 people)": {
+    hourly: {
+      1: 90,
+      2: 160,
+      3: 220,
+      4: 280,
+    },
+    daily: 450,
+  },
+
+  "Wayfarer Dinghy (4 people)": {
+    hourly: {
+      1: 80,
+      2: 140,
+      3: 200,
+      4: 260,
+    },
+    daily: 400,
+  },
+
+  "Kayak (2 people)": {
+    hourly: {
+      1: 20,
+      2: 35,
+      3: 50,
+      4: 60,
+    },
+    daily: 90,
+  },
+
+  "SUP (1 person)": {
+    hourly: {
+      1: 20,
+      2: 35,
+      3: 50,
+      4: 60,
+    },
+    daily: 90,
+  },
+};
