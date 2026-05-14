24a02287437bbcc27776bf5c39cde957dd7c7a6b
DJsailaway <david@stanthony.co.uk>
2026-05-14 19:52:30 +0100

    Create SEO.js

---
+++ components/SEO.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+
+export default function SEO({
+  title,
+  description,
+  image = "/default-og.jpg",
+  url = "https://yourdomain.com"
+}) {
+  return (
+    <Head>
+      {/* Basic */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* Open Graph */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
