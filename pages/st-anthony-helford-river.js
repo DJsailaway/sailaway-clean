3e4f9bc2b901fbcba30701c7311effc3a26b2b1f
DJsailaway <david@stanthony.co.uk>
2026-06-06 16:16:49 +0100

    Update st-anthony-helford-river.js

--- pages/st-anthony-helford-river.js
+++ pages/st-anthony-helford-river.js
@@ -270,7 +270,7 @@ export default function Location() {
     }}
   >
     <iframe
-      src="https://www.google.com/maps?ll=50.08944978350336,-5.1040&z=15&output=embed"
+      src="https://www.google.com/maps?ll=50.08944978350336,-5.1040&z=14&output=embed"
       width="100%"
       height="280"
       style={{ border: 0 }}
