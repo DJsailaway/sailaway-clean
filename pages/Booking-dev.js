6adfe1d113c2fee1b6a0e0c2773c5c246df69831
DJsailaway <david@stanthony.co.uk>
2026-06-14 20:05:07 +0100

    Update Booking-dev.js

--- pages/Booking-dev.js
+++ pages/Booking-dev.js
@@ -1,3 +1,18 @@
 export default function BookingDev() {
-  return <h1>Booking Dev Works</h1>;
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#F8FAFC",
+        padding: "40px",
+        fontFamily: "system-ui"
+      }}
+    >
+      <h1>Booking Wizard V2</h1>
+
+      <p>
+        If you can see this page, the build works.
+      </p>
+    </main>
+  );
 }
