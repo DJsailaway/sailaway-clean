b80086456991d2ab5cf282114400292dbb70bf4f
DJsailaway <david@stanthony.co.uk>
2026-06-14 15:57:57 +0100

    Update Booking-dev.js

--- pages/Booking-dev.js
+++ pages/Booking-dev.js
@@ -1,21 +1,15 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import BookingWizard from "../components/BookingWizardV2/BookingWizard";
 
 export default function BookingDev() {
   return (
-    <>
-      <Navbar />
-      <main
-        style={{
-          maxWidth: "1400px",
-          margin: "0 auto",
-          padding: "40px 20px"
-        }}
-      >
-        <BookingWizard />
-      </main>
-      <Footer />
-    </>
+    <main
+      style={{
+        background: "#F8FAFC",
+        minHeight: "100vh",
+        padding: "32px"
+      }}
+    >
+      <BookingWizard />
+    </main>
   );
 }
