5ea5a405603ae769f949a94766f2b06d529a294b
DJsailaway <david@stanthony.co.uk>
2026-06-14 19:53:38 +0100

    Update Booking-dev.js

--- pages/Booking-dev.js
+++ pages/Booking-dev.js
@@ -1,15 +1,3 @@
-import BookingWizard from "../components/BookingWizardV2/BookingWizard";
-
 export default function BookingDev() {
-  return (
-    <main
-      style={{
-        background: "#F8FAFC",
-        minHeight: "100vh",
-        padding: "32px"
-      }}
-    >
-      <BookingWizard />
-    </main>
-  );
+  return <h1>Booking Dev Works</h1>;
 }
