6510f8338ba18e1624c227bc89e27f23c46a68b8
DJsailaway <david@stanthony.co.uk>
2026-06-16 16:22:21 +0100

    Update WizardLayout.js

--- components/BookingWizardV2/WizardLayout.js
+++ components/BookingWizardV2/WizardLayout.js
@@ -1,8 +1,35 @@
-import PriceSummary from "./PriceSummary";
-import IntentStep from "./steps/IntentStep";
+export default function WizardLayout({
+  children,
+  summary
+}) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "minmax(0, 1fr) 320px",
+        gap: "24px",
+        alignItems: "start",
+        width: "100%"
+      }}
+    >
+      {/* Main wizard content */}
+      <div
+        style={{
+          minWidth: 0
+        }}
+      >
+        {children}
+      </div>
 
-<WizardLayout
-    summary={<PriceSummary />}
->
-    <IntentStep />
-</WizardLayout>
+      {/* Summary panel */}
+      <aside
+        style={{
+          position: "sticky",
+          top: "100px"
+        }}
+      >
+        {summary}
+      </aside>
+    </div>
+  );
+}
