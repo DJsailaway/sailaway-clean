7fc324144db65a6b58a80ae4b42d0a70b2f96f9f
DJsailaway <david@stanthony.co.uk>
2026-06-16 16:18:48 +0100

    Update BookingWizard.js

--- components/BookingWizardV2/BookingWizard.js
+++ components/BookingWizardV2/BookingWizard.js
@@ -21,34 +21,35 @@ export default function BookingWizard() {
       />
 
       <WizardLayout
-        <div style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "16px"
-        }}>
-  <h2 style={{ margin: 0 }}>Choose your activity</h2>
+        summary={<PriceSummary />}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "16px"
+          }}
+        >
+          <h2 style={{ margin: 0 }}>
+            Choose your activity
+          </h2>
 
-  <div style={{
-    padding: "24px",
-    border: "1px solid #E5E7EB",
-    borderRadius: "16px",
-    background: "#fff"
-  }}>
-    Activity cards will go here
-  </div>
-</div>
+          <div
+            style={{
+              padding: "24px",
+              border: "1px solid #E5E7EB",
+              borderRadius: "16px",
+              background: "#fff"
+            }}
+          >
+            Activity cards will go here
+          </div>
+        </div>
       </WizardLayout>
 
       <BottomActionBar
         onBack={() => {}}
         onNext={() => {}}
-        total="£0"
-      />
-    </>
-  );
-}
-        onBack={previousStep}
-        onNext={nextStep}
         total="£0"
       />
     </>
