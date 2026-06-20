909de1ccb038e2fe6e572ac5c25151989b71fbed
DJsailaway <david@stanthony.co.uk>
2026-06-20 19:05:23 +0100

    Create boats.js

---
+++ components/BookingWizardV2/boats.js
@@ -0,0 +1,30 @@
+export const boats = [
+  {
+    id: "plymouth-pilot",
+    activity: "motor",
+    title: "18ft Plymouth Pilot",
+    description: "Comfortable and family-friendly",
+    capacity: "8 people",
+    image: "/images/boats/plymouth-pilot.jpg"
+  },
+
+  {
+    id: "bass-boat",
+    activity: "motor",
+    title: "15ft Bass Boat",
+    description: "Easy to drive and ideal for smaller groups",
+    capacity: "5 people",
+    image: "/images/boats/bass-boat.jpg"
+  },
+
+  {
+    id: "drascombe",
+    activity: "sailing",
+    title: "19ft Drascombe Longboat",
+    description: "Traditional sailing with plenty of space",
+    capacity: "6 people",
+    image: "/images/boats/drascombe.jpg"
+  }
+
+  // ...etc
+];
