6dc7141e692b343fcbc966563c74dd8443425b52
DJsailaway <david@stanthony.co.uk>
2026-06-20 19:32:07 +0100

    Update boats.js

--- components/BookingWizardV2/boats.js
+++ components/BookingWizardV2/boats.js
@@ -2,8 +2,8 @@ export const boats = [
   {
     id: "plymouth-pilot",
     activity: "motor",
-    title: "18ft Plymouth Pilot",
-    description: "Comfortable and family-friendly",
+    title: "Plymouth Pilot",
+    description: "Spacious and stable motor launch",
     capacity: "8 people",
     image: "/images/boats/plymouth-pilot.jpg"
   },
@@ -11,8 +11,8 @@ export const boats = [
   {
     id: "bass-boat",
     activity: "motor",
-    title: "15ft Bass Boat",
-    description: "Easy to drive and ideal for smaller groups",
+    title: "Bass Boat",
+    description: "Simple, nimble and perfect for smaller crews",
     capacity: "5 people",
     image: "/images/boats/bass-boat.jpg"
   },
@@ -20,11 +20,82 @@ export const boats = [
   {
     id: "drascombe",
     activity: "sailing",
-    title: "19ft Drascombe Longboat",
-    description: "Traditional sailing with plenty of space",
+    title: "Drascombe Longboat",
+    description: "Traditional motor-sail dayboat with room to relax",
     capacity: "6 people",
     image: "/images/boats/drascombe.jpg"
   }
 
-  // ...etc
+    {
+    id: "wayfarer",
+    activity: "sailing",
+    title: "Wayfarer",
+    description: "Stable, versatile and wonderfully capable",
+    capacity: "4 people",
+    image: "/images/boats/wayfarer.jpg"
+  },
+
+  {
+    id: "topaz",
+    activity: "sailing",
+    title: "Topaz",
+    description: "Modern sailing that's responsive and fun",
+    capacity: "1-2 people",
+    image: "/images/boats/topaz.jpg"
+  },
+
+  {
+    id: "pico",
+    activity: "sailing",
+    title: "Pico",
+    description: "Simple, sporty and brilliantly versatile",
+    capacity: "1-2 people",
+    image: "/images/boats/pico.jpg"
+  },
+
+  {
+    id: "topper",
+    activity: "sailing",
+    title: "Topper",
+    description: "Pure single-handed sailing",
+    capacity: "1 person",
+    image: "/images/boats/topper.jpg"
+  },
+
+  {
+    id: "anarth",
+    activity: "paddle",
+    title: "Anarth Rowing Dinghy",
+    description: "A local classic rowing dinghy",
+    capacity: "4 people",
+    image: "/images/boats/anarth.jpg"
+  },
+
+  {
+    id: "double-kayak",
+    activity: "paddle",
+    title: "Double Kayak",
+    description: "Perfect for exploring as a pair",
+    capacity: "2 people",
+    image: "/images/boats/double-kayak.jpg"
+  },
+
+  {
+    id: "single-kayak",
+    activity: "paddle",
+    title: "Single Kayak",
+    description: "Quiet independant exploration",
+    capacity: "2 people",
+    image: "/images/boats/single-kayak.jpg"
+  },
+
+    {
+    id: "paddleboard",
+    activity: "paddle",
+    title: "Stand-Up Paddleboard",
+    description: "Get closer to the water",
+    capacity: "1 person",
+    image: "/images/boats/paddleboard.jpg"
+  },
+      
 ];
