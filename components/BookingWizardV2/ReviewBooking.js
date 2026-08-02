24b8dc58b8a9907f9af8ea6f34a07398246b859d
DJsailaway <david@stanthony.co.uk>
2026-08-02 20:16:31 +0100

    Update ReviewBooking.js

--- components/BookingWizardV2/ReviewBooking.js
+++ components/BookingWizardV2/ReviewBooking.js
@@ -1,4 +1,4 @@
-export default function ReviewBooking({ reviewData, onEditStep }) {
+export default function ReviewBooking({  reviewData,  onEditStep,  onSubmitRequest,}) {
   if (!reviewData) {
     return null;
   }
